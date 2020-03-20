d084d36fcd93b313e5c7a965257411a0d3fcb80a
gennadiy123 <shelukhin_g@ukr.net>
2020-03-20 15:41:48 +0200

    Updates

---
+++ precache-manifest.4cc43a2dbbd96e6f1569393de9c46789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cde9f72841f8dd241fa1ec6a0c418e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "47df2793f2d25cccfe0e",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6d048306.chunk.css"
+  },
+  {
+    "revision": "b531a68a3c838dd223e9",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6ae94c76.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6ae94c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47df2793f2d25cccfe0e",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.bdd1cd34.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
